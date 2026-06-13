2ef85f1c47f3e5f0d486ba0f16b7acaaedef2587
Sanan Malik <iamsanan50@gmail.com>
2026-06-13 22:25:40 +0500

    update

---
+++ STEMPilotAI.Core/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace STEMPilotAI.Core.Entities;
+
+public class User
+{
+    public int UserId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+}
