2c98899d868f78c50a0ad18aea4eb59eaf92dc56
Sanan Malik <iamsanan50@gmail.com>
2026-06-13 23:32:27 +0500

    Updates

---
+++ STEMPilotAI.API/Controllers/ResultsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using STEMPilotAI.Infrastructure.Data;
+
+namespace STEMPilotAI.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ResultsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ResultsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("user/{userId}")]
+    public IActionResult GetUserResults(int userId)
+    {
+        var results = _context.QuizResults
+            .Where(r => r.UserId == userId)
+            .OrderByDescending(r => r.AttemptDate)
+            .ToList();
+
+        return Ok(results);
+    }
+}
