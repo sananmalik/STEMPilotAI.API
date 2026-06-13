12ac9bfa4ef2543cfd598372a105a31aa832d806
Sanan Malik <iamsanan50@gmail.com>
2026-06-13 22:52:15 +0500

    u

---
+++ STEMPilotAI.Core/Entities/Question.cs
@@ -0,0 +1,22 @@
+﻿namespace STEMPilotAI.Core.Entities;
+
+public class Question
+{
+    public int QuestionId { get; set; }
+
+    public int QuizId { get; set; }
+
+    public string QuestionText { get; set; } = string.Empty;
+
+    public string OptionA { get; set; } = string.Empty;
+
+    public string OptionB { get; set; } = string.Empty;
+
+    public string OptionC { get; set; } = string.Empty;
+
+    public string OptionD { get; set; } = string.Empty;
+
+    public string CorrectAnswer { get; set; } = string.Empty;
+
+    public Quiz? Quiz { get; set; }
+}
