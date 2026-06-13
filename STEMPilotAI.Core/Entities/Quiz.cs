c6048ba28eea95caa3450e7130600f743bb495fc
Sanan Malik <iamsanan50@gmail.com>
2026-06-13 22:51:33 +0500

    u

---
+++ STEMPilotAI.Core/Entities/Quiz.cs
@@ -0,0 +1,12 @@
+﻿namespace STEMPilotAI.Core.Entities;
+
+public class Quiz
+{
+    public int QuizId { get; set; }
+
+    public int SubjectId { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public Subject? Subject { get; set; }
+}
