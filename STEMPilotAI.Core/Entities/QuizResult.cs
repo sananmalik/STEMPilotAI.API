b44e3ec80f3b3b8f942abc93c87ef14c29d399af
Sanan Malik <iamsanan50@gmail.com>
2026-06-13 22:57:33 +0500

    database update

---
+++ STEMPilotAI.Core/Entities/QuizResult.cs
@@ -0,0 +1,12 @@
+﻿public class QuizResult
+{
+    public int QuizResultId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int SubjectId { get; set; }
+
+    public int Score { get; set; }
+
+    public DateTime AttemptDate { get; set; }
+}
---
+++ STEMPilotAI.Infrastructure/Migrations/20260613175710_AddQuestionAndQuizResultTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace STEMPilotAI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuestionAndQuizResultTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
